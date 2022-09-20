8c2d31b4d6743d3e2a3b0f69ef8ed4a283ab2cf4
Nicholas <nc_chau@hotmail.com>
2022-09-20 16:40:37 +0000

    refactor getAllReservations for psql

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -88,10 +88,6 @@ const addUser =  function(user) {
     console.log(err.message);
   });
 
-  // const userId = Object.keys(users).length + 1;
-  // user.id = userId;
-  // users[userId] = user;
-  // return Promise.resolve(user);
 }
 exports.addUser = addUser;
 
@@ -103,7 +99,26 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+
+  const queryString = `
+  SELECT reservations.id as id, properties.*, start_date, avg(property_reviews.rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date;
+  `;
+
+return pool
+  .query(queryString, [`${guest_id}`])
+  .then(result => {
+      return result.rows;
+    })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
 }
 exports.getAllReservations = getAllReservations;
 
